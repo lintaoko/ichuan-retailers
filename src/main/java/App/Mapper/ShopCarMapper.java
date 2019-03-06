c5fcf0bb704df34216a8db69cd032f76932956b7
lintaoko <1244275215@qq.com>
2019-03-06 21:11:11 +0800

    1111

--- src/main/java/App/Mapper/ShopCarMapper.java
+++ src/main/java/App/Mapper/ShopCarMapper.java
@@ -1,17 +1,22 @@
 package App.Mapper;
 
+import App.Domain.ShopCar;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 public interface ShopCarMapper {
+    //购物车查询
+    @Select("select * form shopcar where UserId=#{UserId}")
+    ShopCar queryShopCarByUserId(@Param("UserId")Integer userId);
     //购物车添加
     @Insert("insert into shopcar(UserId,GoodsName,GoodsImg,GoodsPrice,PurchaseQuantity) VALUES(#{UserId},#{GoodsName},#{GoodsImg},#{GoodsPrice},#{PurchaseQuantity})")
-    int shopCarInsert (@Param("UserId") Integer userId,@Param("GoodsName")String goodsName,@Param("GoodsImg")String goodsImg,@Param("GoodsPrice")Double goodsPrice,@Param("PurchaseQuantity") Integer purchaseQuantity);
+    Integer shopCarInsert (@Param("UserId") Integer userId,@Param("GoodsName")String goodsName,@Param("GoodsImg")String goodsImg,@Param("GoodsPrice")Double goodsPrice,@Param("PurchaseQuantity") Integer purchaseQuantity);
     //购物车单项删除
     @Delete("delete shopcar where ShopCarId=#{ShopCarId}")
-    int shopCarDeleteByShopCarId(@Param("ShopCarId") Integer shopCarId);
+    Integer shopCarDeleteByShopCarId(@Param("ShopCarId") Integer shopCarId);
     //购物车清空
     @Delete("delete shopcar where UserId=#{UserId}")
-    int shopCarDeleteByUserId(@Param("UserId")Integer userId);
+    Integer shopCarDeleteByUserId(@Param("UserId")Integer userId);
 }
--- src/main/java/App/Service/ShopCarService.java
+++ src/main/java/App/Service/ShopCarService.java
@@ -1,4 +1,37 @@
 package App.Service;
 
+import App.Domain.ShopCar;
+import App.Mapper.ShopCarMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class ShopCarService {
+
+    @Autowired
+    ShopCarMapper shopCarMapper;
+
+    //购物车查询
+    public  ShopCar queryShopCarByUserId(Integer userId){
+       ShopCar selectResult=shopCarMapper.queryShopCarByUserId(userId);
+       return  selectResult;
+    }
+    //购物车添加
+    public Integer shopCarInsert (Integer userId,String goodsName,String goodsImg,Double goodsPrice, Integer purchaseQuantity){
+        Integer insertResult=shopCarMapper.shopCarInsert(userId, goodsName, goodsImg, goodsPrice, purchaseQuantity);
+        return insertResult;
+    }
+    //购物车单项删除
+    public Integer shopCarDeleteByShopCarId(Integer shopCarId){
+        Integer deleteResult=shopCarMapper.shopCarDeleteByShopCarId(shopCarId);
+        return deleteResult;
+    }
+    //购物车清空
+    public Integer shopCarDeleteByUserId(Integer userId){
+        Integer deleteResult=shopCarMapper.shopCarDeleteByUserId(userId);
+        return deleteResult;
+    }
 }
